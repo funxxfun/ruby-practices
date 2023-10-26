02147dba1133aa0dc33c7645e350d5d58aed3be4
funxxfun <23shirasawa@gmail.com>
2023-10-26 12:26:26 +0900

    メソッドをループ処理の外に出し、putsでの出力箇所を変更した

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,14 +1,18 @@
-(1..20).each do |number|
-  def fizzbuzz(number)
-    if number % 15  == 0
-      puts 'FizzBuzz'
-    elsif number % 3 == 0
-      puts 'Fizz'
-    elsif number % 5 == 0
-      puts 'Buzz'
-    else
-      puts number
-    end
+def fizzbuzz(number)
+  # 3でも5でも割り切れる場合
+  if number % 15  == 0
+    'FizzBuzz'
+  # 3で割り切れる場合
+  elsif number % 3 == 0
+    'Fizz'
+  # 5で割り切れる場合
+  elsif number % 5 == 0
+    'Buzz'
+  else
+    number
   end
+end
+
+(1..20).each do |number|
   puts fizzbuzz(number)
 end
