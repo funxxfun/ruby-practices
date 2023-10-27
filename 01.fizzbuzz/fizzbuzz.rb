904c6bc7855a3c7d38fe07bb3bd4e568ed1eff14
funxxfun <23shirasawa@gmail.com>
2023-10-28 05:29:18 +0900

    不要なコメントを削除した

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,11 +1,8 @@
 def fizzbuzz(number)
-  # 3でも5でも割り切れる場合
   if number % 15  == 0
     'FizzBuzz'
-  # 3で割り切れる場合
   elsif number % 3 == 0
     'Fizz'
-  # 5で割り切れる場合
   elsif number % 5 == 0
     'Buzz'
   else
