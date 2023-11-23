3838e2bdebaf102edef675a674b4b2eb9370162a
funxxfun <23shirasawa@gmail.com>
2023-11-23 10:31:41 +0900

    １〜９フレームの合計方法と、10フレームの合計方法を条件分岐で分けて記述

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -22,15 +22,19 @@ end
 
 point = 0
 frames.each_with_index do |frame, index|
-  point += if frame[0] == 10 && frames[index + 1][0] == 10 # ストライクが続いた時
-             frame.sum + frames[index + 1][0] + frames[index + 2][0] + frames[index + 2][1]
-           elsif frame[0] == 10 # ストライクの時
-             frame.sum + frames[index + 1][0] + frames[index + 1][1]
-           elsif frame.sum == 10 # スペアの時
-             frame.sum + frames[index + 1][0]
-           else
-             frame.sum
-           end
+  if index <= 8 # 1~9フレーム
+    point += if frame[0] == 10 && frames[index + 1][0] == 10 # 連続ストライクの時
+               frame.sum + frames[index + 1][0] + frames[index + 2][0] + frames[index + 2][1]
+             elsif frame[0] == 10 # 連続しないストライクの時
+               frame.sum + frames[index + 1][0] + frames[index + 1][1]
+             elsif frame.sum == 10 # スペアの時
+               frame.sum + frames[index + 1][0]
+             else
+               frame.sum
+             end
+  end
+
+  point += frame.sum if index >= 9 # 10フレーム
 end
 p frames
 p point
